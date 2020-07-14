14a99b82a9e2759a35260e25da54f21d39191a21
osha7 <ogroetz@gmail.com>
2020-07-14 16:23:20 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+
+main.remove()
+// document.querySelector("main#main").remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.innerHTML = "Osha is the champion";
+document.body.appendChild(newHeader);
