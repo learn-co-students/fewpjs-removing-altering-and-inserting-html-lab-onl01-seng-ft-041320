c5986e89e33c33843f858cf1e0c8ea1b71e8c4f1
jaesonwatts <illender.the.great@gmail.com>
2020-07-27 11:45:51 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+var element = document.querySelector('main');
+element.remove();
+
+var newHeader = document.createElement('h1');
+console.log(newHeader.className)
+newHeader.id = 'victory'
+newHeader.innerHTML = 'Jaeson is the champion'
+newHeader.className = 'victory';
